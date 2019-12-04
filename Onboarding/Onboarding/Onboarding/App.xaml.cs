eab3eb3741c8cc98a124392a773808e0d5f3b287
Thinh Tran <48557139+ThinhTranA@users.noreply.github.com>
2019-12-04 21:02:31 +0800

    init

---
+++ Onboarding/Onboarding/Onboarding/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Onboarding.Views;
+using Xamarin.Forms;
+
+namespace Onboarding
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new OnboardingPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
